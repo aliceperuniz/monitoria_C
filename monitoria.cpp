77f723d8593c03dc01b98c6c8c8d60546c7f4d26
Alice Peruniz Oliveira <apo2@WINDOWS.CIN.UFPE.BR>
2022-12-21 13:04:28 -0300

    second commit

--- monitoria.cpp
+++ monitoria.cpp
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define  CONST 6
+#define CONST 6
+#define TAM 5
 #include <iostream>
 
-//&
+
+void modifica_array(int *arr, int valor){ 
+    for(int i = 0; i < TAM; i++) {
+        *((*arr) + i * sizeof(int)) = valor;  // == x[i]
+    }
+}
+
+// &
 //*
 int main() {
     int *x;
-    x = (int *) malloc(sizeof(int))
+    x = (int *) malloc(TAM * sizeof(int))
     (*x) = 10; // modificando o endereço, não o x
-    print("%d", &x);
+    printf("%d", *x);
     std::cout << &x << std::endl;
     return 0;
+    modifica_array(x, valor)
+    for(int i = 0; i < TAAM; i++) {
+        printf("%d", x[i]);
+    }
+    return 0;
 }
+
